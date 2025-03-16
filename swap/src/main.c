1170d74bdb4a76da6b9ca6503c469549ff8c00b6
Alessandro Nardinelli <anardine.ef@gmail.com>
2025-03-16 18:50:28 -0300

    implementation done

--- swap/src/main.c
+++ swap/src/main.c
@@ -13,7 +13,11 @@ int main() {
     int *pToNum1 = &num1; 
     int *pToNum2 = &num2; 
 
+    printf("numbers before swaping are %d and %d \n", num1, num2);
+
     swapNums(pToNum1, pToNum2);
+
+    printf("numbers after swaping are %d and %d", num1, num2);
 
     return 0;
 }
