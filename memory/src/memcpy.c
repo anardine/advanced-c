ef04cbb1c89f9226c002bd980a6b2330e85e6822
Alessandro Nardinelli <anardine.ef@gmail.com>
2025-03-17 12:11:11 -0300

    added memcpy func implementation

---
+++ memory/src/memcpy.c
@@ -0,0 +1,13 @@
+#include <sys/_types/_size_t.h>
+ // memory copy definition and implementation
+
+
+ void memcpy(double *source, int *dest, size_t numBytes) {
+    
+    *dest = 0;
+    *dest = (int)*source | *dest;
+
+
+ } 
+
+
